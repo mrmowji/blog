3ce8d80e5c0c7ef54eaeb9978770837dfa0143db
Mojtaba Javan <javan.mojtaba@gmail.com>
2020-04-13 06:37:47 +0430

    Add date formatting filters

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,16 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/manifest.json");
   eleventyConfig.addPassthroughCopy("src/CNAME");
 
+  eleventyConfig.addFilter("persianDate", function (value) {
+    return value.toISOString().split("T")[0].replace(/\-/g, "/");
+    //return value.toLocaleDateString('fa-IR');
+  });
+
+  eleventyConfig.addFilter("isoDate", function (value) {
+    return value.toISOString();
+    //return value.toLocaleDateString('fa-IR');
+  });
+
   return {
     dir: {
       input: "src",
